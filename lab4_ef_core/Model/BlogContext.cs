9595d0b2fd3070ab7b6e0ac7a03c623c358b08ad
Nick Chu <pig520421@gmail.com>
2022-12-08 16:02:04 +0800

    使用efcore

---
+++ lab4_ef_core/Model/BlogContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Model;
+public class BlogContext : DbContext
+{
+    public DbSet<Blog>? Blogs { get; set; }
+    public DbSet<Post>? Posts { get; set; }
+    public string DbPath { get; }
+    public BlogContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path,"lab4.db");
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options) 
+    => options.UseSqlite($"Data Source={DbPath}");
+}
--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Model;
+using var db = new BlogContext();
+Console.WriteLine($"資料庫的路徑是:{db.DbPath}");
+
+db.Add(new Blog{Uri = "http://www.uuu.com.tw"});
+db.Add(new Blog{Uri = "http://www.tibame.com"});
+db.Add(new Blog{Uri = "http://hahow.in"});
+db.SaveChanges();
+var blogs = db.Blogs!.OrderBy(x => x.BlogId);
+foreach(var item in blogs )
+{
+    Console.WriteLine($"{item.BlogId},{item.Uri}");
+}
