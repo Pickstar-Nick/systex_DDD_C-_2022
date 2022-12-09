2e83c61ab3628e79ab72c8e59d39e3ee2ab60a24
Nick Chu <pig520421@gmail.com>
2022-12-09 13:46:40 +0800

    增加兩個端點

---
+++ lab6_together/Together.Api/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Together.Api.Controllers;
+[ApiController]
+[Route("api")]
+public class EventController:ControllerBase
+{
+    [Route("add-event")]
+    public IActionResult AddEvent()
+    {
+        return Ok();
+    }
+    [Route("query-event")]
+    public IActionResult QueryEvent()
+    {
+        return Ok();
+    }
+}
