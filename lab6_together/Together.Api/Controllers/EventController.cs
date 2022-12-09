e5a4f712c866808e065491cb349018b65241c0c6
Nick Chu <pig520421@gmail.com>
2022-12-09 14:30:34 +0800

    使用record做request

--- lab6_together/Together.Api/Controllers/EventController.cs
+++ lab6_together/Together.Api/Controllers/EventController.cs
@@ -1,17 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using Together.Contract.Controller;
+
 namespace Together.Api.Controllers;
 [ApiController]
 [Route("api")]
 public class EventController:ControllerBase
 {
-    [Route("add-event")]
-    public IActionResult AddEvent()
+    [HttpPost("add-event")]
+    public IActionResult AddEvent(AddEventRequest request)
     {
-        return Ok();
+        return Ok(request);
     }
-    [Route("query-event")]
-    public IActionResult QueryEvent()
+    [HttpGet("query-event")]
+    public IActionResult QueryEvent(QueryEventRequest request)
     {
-        return Ok();
+        return Ok(request);
     }
 }
